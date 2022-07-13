dfeb8301446328314c7c80cc7f58fcdf74969cdd
Pingggggzi <gutenachrichten1015@gmail.com>
2022-07-13 17:58:46 +0800

    Update facialRecognition page

--- lib/screens/facialRecogition.dart
+++ lib/screens/facialRecogition.dart
@@ -22,7 +22,85 @@ class _FacialRecognitionState extends State<FacialRecognition> {
       ),
       body: Container(
         decoration: BoxDecoration(color: Colors.grey[900]),
-        child: ListView(),
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'cto',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 3, 84, 95),
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    's',
+                    style: TextStyle(
+                        color: Colors.red[900],
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                Text(
+                  'Record a 4 seconds selfie video',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '1.Enable video capture permission',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                Text(
+                  "2.Tap 'Start Recording' when ready",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ]),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+              height: 240,
+              width: double.infinity,
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            ),
+            Container(
+              margin: EdgeInsets.all(100),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.record_voice_over,
+                      color: Color.fromARGB(255, 107, 227, 237),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Start Recording',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 107, 227, 237),
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
